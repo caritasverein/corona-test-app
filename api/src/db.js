d97cfa12dd9bf1bd9a79b4cd7b854fa615e1efc8
Luca Haneklau <git@luca.lsys.ac>
2021-04-25 02:46:26 +0200

    fixed api db module

--- api/src/db.js
+++ api/src/db.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql2/promise');
+import mysql from 'mysql2/promise';
 
-const db = mysql.createPool({
+export const db = mysql.createPool({
   host: 'db',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   timezone: 'Z',
 });
-module.exports.db = db;
+export default db;
 
 /*
 SELECT
